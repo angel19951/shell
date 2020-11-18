112793d099955182842d0253e06c9602e8344061
angel19951 <2384@holbertonschool.com>
2020-11-18 12:42:32 +0000

    Looks for command in all directories of PATH | C

---
+++ _access.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+#include "_string.h"
+#include "string1.c"
+#include "string2.c"
+#include "getenv.c"
+
+/**
+ * main - looks for a comman in all directories of PATH
+ * @ac: ammount of attributes passed to main
+ * @argv: arguments passed to main
+ * Return: 1 if success || -1 if fail
+ */
+int main(__attribute__((unused))int ac, char *argv[])
+{
+	struct stat st;
+	char *check;
+	char *path = _getenv("PATH");
+	char **tokens = _strtok(path, ":");
+
+	while (*tokens != NULL)
+	{
+		check = concatenator(3, tokens, "/", argv[1]);
+		if (stat(check, &st) == 0)
+		{
+			printf("FOUND\n");
+			exit(0);
+		}
+		else
+		{
+			return(-1);
+		}
+		tokens++;
+	}
+	return (0);
+}
