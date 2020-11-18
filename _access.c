787da722519f2ef796ad93657beb7945f37dc453
angel19951 <2384@holbertonschool.com>
2020-11-18 16:50:46 +0000

    Fixed error where program exits prematurely, as well freeing the variable holding our directory

--- _access.c
+++ _access.c
@@ -13,23 +13,26 @@
 int main(__attribute__((unused))int ac, char *argv[])
 {
 	struct stat st;
-	char *check;
+	char *check = NULL;
 	char *path = _getenv("PATH");
 	char **tokens = _strtok(path, ":");
 
 	while (*tokens != NULL)
 	{
-		check = concatenator(3, tokens, "/", argv[1]);
+		check = concatenator(3, *tokens, "/", argv[1]);
 		if (stat(check, &st) == 0)
 		{
-			printf("FOUND\n");
+			printf("FOUND in %s\n", check);
+			free(check);
 			exit(0);
 		}
 		else
 		{
-			return(-1);
+			printf("Not found in %s\n", check);
 		}
 		tokens++;
+		if (check)
+			free(check);
 	}
 	return (0);
 }
